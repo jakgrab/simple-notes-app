bc5eeef496e7d3b5e12e99bc007d48cb379f3507
jakub.grabowski <kubagrabowski1903@gmail.com>
2024-10-31 19:06:14 +0100

    Create basic home screen, add localizations

---
+++ lib/config/theme/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData.light().copyWith(
+  scaffoldBackgroundColor: Colors.white,
+  appBarTheme: const AppBarTheme(
+    foregroundColor: Colors.white,
+    backgroundColor: Colors.brown,
+  ),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    foregroundColor: Colors.white,
+    backgroundColor: Colors.brown,
+  ),
+);
